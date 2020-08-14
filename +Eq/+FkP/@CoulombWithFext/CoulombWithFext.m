3c579c5e6150f5865d97821834ec49d800559c45
okatsn <49422823+okatsn@users.noreply.github.com>
2020-08-14 11:49:04 +0800

    Update CoulombWithFext.m
    - now it can calculates the expectation of positive and negative distribution of steady-state slip velocities separately
    - support directly return values

--- +Eq/+FkP/@CoulombWithFext/CoulombWithFext.m
+++ +Eq/+FkP/@CoulombWithFext/CoulombWithFext.m
@@ -4,19 +4,46 @@ classdef CoulombWithFext
 % other artificial error.
 
 methods (Static)
-    function v_expectation = v_expectation()
+    function [v_avg,v_avg_neg,v_avg_pos] = v_expectation(varargin)
+        v_avg = @(F_C,F_ext,D) -2*D*F_ext/(F_ext^2 - F_C^2);
         % <v> = \int_{-\infty}^{+\infty} v p(v) dv ; see draft and guide.md
-        v_expectation = @(F_C,F_ext,D) -2*D*F_ext/(F_ext^2 - F_C^2);
+        v_avg_neg = @(F_C,F_ext,D) -D*(F_C-F_ext)/((2*F_C)*(F_ext+F_C));
+        % <v-> = \int_{0}^{+\infty} v p(v) dv 
+        v_avg_pos = @(F_C,F_ext,D) -D*(F_C+F_ext)/((2*F_C)*(F_ext-F_C));
+        % <v+> = \int_{-\infty}^{0} v p(v) dv 
+        if nargin == 0
+            return
+        elseif nargin == 3
+            v_avg = v_avg(varargin{:});
+            v_avg_neg = v_avg_neg(varargin{:});
+            v_avg_pos = v_avg_pos(varargin{:});
+        else
+            error('invalid number of input argument');
+        end
     end
-    function P0 = P0()
+    function P0 = P0(varargin)
         % coefficient for normalization
         P0 = @(r,Fc,D) (Fc^2- r^2)/(-2*r*D); 
         % see '(2020) Solve SDE for DRY friction.mw'
+        if nargin == 0
+            return
+        elseif nargin == 3
+            P0 = P0(varargin{:});
+        else
+            error('invalid number of input argument');
+        end
     end
     
-    function [FitFunction] = Pst()
+    function [FitFunction] = Pst(varargin)
         % see Wu2020 (not submit yet)
         FitFunction = @(P0,r,Fc,D,v) P0*exp(Fc*v/D).*exp(-r*v.*sign(v)/D);
+        if nargin == 0
+            return
+        elseif nargin == 4
+            FitFunction = FitFunction(varargin{:});
+        else
+            error('invalid number of input argument');
+        end
     end
     
     
@@ -26,12 +53,20 @@ methods (Static)
 %         FitFunction = @(v,r,Fc,D) P0(r,Fc,D)*exp(Fc*v/D).*exp(-r*v.*sign(v)/D);
 %     end
     
-    function [f_neg,f_pos] = Pst_cases() % another equivalent form of Pst_CoulombFext_Wu2020
+    function [f_neg,f_pos] = Pst_cases(varargin) % another equivalent form of Pst_CoulombFext_Wu2020
         f_neg = @(P0,r,Fc,D,v) P0*exp((Fc + r)*v/D); % for v <= 0
         f_pos = @(P0,r,Fc,D,v) P0*exp((Fc - r)*v/D); % for v > 0
+        if nargin == 0
+            return
+        elseif nargin == 4
+            f_neg = f_neg(varargin{:});
+            f_pos = f_pos(varargin{:});
+        else
+            error('invalid number of input argument');
+        end
     end
     
-    function [TEX_neg,TEX_pos] = trPst_cases()
+    function [TEX_neg,TEX_pos] = trPst_cases(varargin)
         fn = @(P0,r,Fc,D,v) P0*exp((Fc + r)*v/D); % f_neg, for v <= 0
         fp = @(P0,r,Fc,D,v) P0*exp((Fc - r)*v/D); % f_pos, for v > 0
         int_fn = @(P0,r,Fc,D,v) P0*D/(Fc+r)*exp((Fc + r)*v/D); % integral form
@@ -40,6 +75,15 @@ methods (Static)
         TEX_neg = @(vmin,vmax,P0,r,Fc,D,v) fn(P0,r,Fc,D,v)/Pin(vmin,vmax,P0,r,Fc,D);
         TEX_pos = @(vmin,vmax,P0,r,Fc,D,v) fp(P0,r,Fc,D,v)/Pin(vmin,vmax,P0,r,Fc,D);
 %         f_pos = @(v,P0,r,Fc,D) P0*exp((Fc - r)*v/D)/(((P0*D/(Fc+r)-P0*D/(Fc+r)*exp((Fc + r)*vmin/D))-()); % for v > 0
+
+        if nargin == 0
+            return
+        elseif nargin == 7
+            TEX_neg = TEX_neg(varargin{:});
+            TEX_pos = TEX_pos(varargin{:});
+        else
+            error('invalid number of input argument');
+        end
     end
 %     P0*D/(Fc-r)*exp((Fc - r)*v/D)-P0*D/(Fc-r)
 
