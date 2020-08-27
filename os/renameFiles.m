c5c436666387153b6a50f6af1e793a013e8f840e
okatsn <49422823+okatsn@users.noreply.github.com>
2020-08-27 11:33:16 +0800

    Update renameFiles.m
    - Support for renaming files under a specific folder (namely 'oldfolder'), and the renamed files can be chosen to save in 'newfolder' that has the same structure as the 'oldfolder'.

--- os/renameFiles.m
+++ os/renameFiles.m
@@ -4,24 +4,55 @@ function renameFiles(regexppattern,sprintfformat,oldpathlist,varargin)
 % Example, for file name '[Stat_thr=5]_Ts_D=5.34_r=1.00_Fc=1.00_Type=dry.mat', we have
 %     regexppattern = '((?<=(thr|D|r|Fc|Type)=)(\d+\.?\d*|\w+))';
 %     sprintfformat = '[Stat]_thr[%s]_D[%s]_r[%s]_Fc[%s]_Type[%s].mat';
-%     regexppattern for regexp using 'match':
-%         renameFiles(regexppattern,sprintfformat,oldpathlist); 
-%     regexppattern for regexp using 'names':
-%         renameFiles(regexppattern,sprintfformat,oldpathlist,'names'); 
-    
+%
+%     renameFiles(regexppattern,sprintfformat,oldpathlist); 
+%     - regexppattern for regexp using 'match':
+%     
+%     renameFiles(regexppattern,sprintfformat,oldpathlist,'names'); 
+%     - regexppattern for regexp using 'names'.
+%
+%     Noted that 'names' or 'match' for specifying 
+%     regexp method should always be the last argument.
+%
+% Example2, do datalist inside the renameFiles function:
+%     targetfolder = 'F:\GeoMag (main)\CWBMagnetism_1';
+%     renameFiles(regexppattern,sprintfformat,targetfolder);
+%
+%     newfoldername = 'CWBMagnetism_2'; 
+%     renameFiles(regexppattern,sprintfformat,targetfolder,newfoldername);
+%     - output the renamed files to '..\CWBMagnetism_2\..' 
+%       instead of its original e.g. '..\CWBMagnetism_1\..'
+%     - Noted that newfoldername must be the 4th arguments (i.e. varargin{1})
+
 % future work: add support to regexp(...,'names');
-if nargin>3 && strcmp(varargin{1},'names')
-    regdo = 'names';
-    regout_is_struct = true;
-else
-    regdo = 'match';
-    regout_is_struct = false;
+regdo = 'match';
+createnewfoldertree = false;
+regout_is_struct = false; 
+if nargin>3
+    if strcmp(varargin{end},'names')
+        regdo = 'names';
+        regout_is_struct = true;
+        varargin(end) = [];
+    end
+    
+    if strcmp(varargin{end}, 'match')
+        varargin(end) = [];
+    end
+    
+    if ischar(varargin{1}) && isfolder(oldpathlist)
+        newfoldername = varargin{1};
+        createnewfoldertree = true;
+        varargin(1) = [];
+    end
+    
 end
+
 
 p = inputParser;
 addParameter(p,'Order',0);
 addParameter(p,'FunctionHandle',@(old,new) copyfile(old,new));
 addParameter(p,'Delete',false);
+% addParameter(p,'dirPattern','*'); % pattern for finding files. e.g. *.txt for all files end with '.txt'
 parse(p,varargin{:});
 Order = p.Results.Order;
 fileoperation = p.Results.FunctionHandle;
@@ -40,9 +71,46 @@ if ~isequal(Order,0)
     end
 end
 
-for i = 1:size(oldpathlist,1)
+switch class(oldpathlist)
+    case 'cell'
+        
+    case 'char'
+        if ~isfolder(oldpathlist)
+            error('This is not a valid folder.');
+        end
+        fdir0 = oldpathlist;
+        if createnewfoldertree
+            rootfolderloc = length(strsplit(fdir0,filesep)); % location/level of the root folder in the full path.
+%             [~,oldfoldername,~] = fileparts(fdir0);
+            oldfolderstruct = datalist('*',fdir0,'Search','**FolderOnly').fullpath;
+            disp('[renameFiles] Creating new folder tree...');
+            for i = 1:length(oldfolderstruct)
+                folder_i = oldfolderstruct{i};
+                dirseries = strsplit(folder_i,filesep);
+                dirseries{rootfolderloc} = newfoldername;
+                mkdir(fullfile(dirseries{:}));
+            end
+        end
+        oldpathlist = datalist('*',fdir0,'Search','**FileOnly').fullpath;
+    otherwise
+        error('unsupported types. oldpathlist has to be an cell array or a folder path.')
+end
+total_iters = size(oldpathlist,1);
+
+if total_iters>5000
+tic; H = timeLeft0(total_iters,'Renaming files...',5000);    
+end
+
+for i = 1:total_iters
+    if total_iters>5000; [H] = timeLeft1(toc,i,H); end
     oldpath = oldpathlist{i};
     [fdir,fname,fext] = fileparts(oldpath);
+    if createnewfoldertree
+        fdirsplitted = strsplit(fdir,filesep);
+        fdirsplitted{rootfolderloc} = newfoldername;
+        fdir = fullfile(fdirsplitted{:});
+    end
+    
     fname_ext = [fname,fext];
     regout = regexp(fname_ext,regexppattern,regdo);
     if regout_is_struct
@@ -68,10 +136,10 @@ for i = 1:size(oldpathlist,1)
         delete(oldpath);
     end
 end
-
+if total_iters>5000; delete(H.waitbarHandle); end
 
 if skippedcount > 0
-    headstr = sprintf('The following %d files not copied/moved:',skippedcount);
+    headstr = sprintf('The following %d files not copied/renamed:',skippedcount);
     warning('%s\n',headstr,skippedfiles{:});
 end
 
