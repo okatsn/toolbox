bf8ece1058000b28ac05f425d3a87ca1e551a065
okatsn <49422823+okatsn@users.noreply.github.com>
2020-08-31 11:39:51 +0800

    Update RootFolder.m

--- os/RootFolder.m
+++ os/RootFolder.m
@@ -1,8 +1,13 @@
-function [varargout] = RootFolder
+function [varargout] = RootFolder(varargin)
 % [root0,pf_iters,nickname] = RootFolder
+% [root0,pf_iters,nickname] = RootFolder(0) % don't print information.
 % root0: the anchor point for relative paths/directories for different devices.
 % pf_iters: number of maximum thread for parfor
 % nickname: nick name for the device
+do_print = true;
+if nargin > 0
+    do_print = varargin{end};
+end
 
 pf_iters = feature('numcores'); % number of maximum threads for parfor
 linux_ncu = 'Linux_NCU_CGRG';
@@ -28,9 +33,9 @@ switch computer_name
     otherwise
         error('Error in specifying root folder. See RootFolder');
 end
-
-fprintf('%d cores available. This is %s\n',pf_iters,nickname);
-
+if do_print
+    fprintf('%d cores available. This is %s\n',pf_iters,nickname);
+end
 varargout{1} = root0;
 if nargout>1
     varargout{2} = pf_iters;
