3412084504adeaa74a75365c3f899432f2ae1a74
okatsn <49422823+okatsn@users.noreply.github.com>
2020-08-31 11:39:09 +0800

    Critical upgrade

--- fillNaN.m
+++ fillNaN.m
@@ -1,44 +1,63 @@
 function [Y0] = fillNaN(Y0)
 % replace NaNs with linear interpolation dithered by noise.
-% This function is used in CWB MagTIP project only.
-% 應該是沒問題了→warning('fillNaN may need some attention (about the X, Xmissing in interp1. Check doc interp1)');
-% Y0 = YBeforeDetrend';
-[NoY, NoC] = size(Y0);
-if size(Y0,1) ==1
-    Y0 = Y0';
-    NoY = NoC;
-end
 
-% Y0(2000:3000) = NaN;
-% Y0(70000:80000) = NaN;
-
+Y0 = Y0(:);
+NoY = length(Y0);
 Y = Y0;
 
 X0 = [1:NoY]';
 X = X0;
 
-Xmissing = find(isnan(Y));
-% Xmissing = [2,3,5:10,17:18,22:25,29]';
-Y(Xmissing) = [];
-X(Xmissing) = [];
-Yintp = interp1(X,Y,Xmissing,'linear');
-% figure;
-% plot(X,Y,'bo')
-% hold on
-% plot(Xmissing,Yintp,'ro');
+YisNaN = isnan(Y);
+Y(YisNaN) = [];
+X(YisNaN) = [];
+XmissingInd = YisNaN; % ~ismember(X0,X);
+Xintp = X0(XmissingInd); 
 
+std1 = nanmedian(movstd(Y0,250)); % to estimate the std for the nosie.
+try
+    Yintp = interp1(X,Y,Xintp,'linear');
+catch ME
+    if length(X)<2
+        % Interpolation requires at least two sample points in each dimension.
+        stillnan = isnan(Y0); % if NaN starts from the beginning, Y_intp is NaN, 
+        Y0(stillnan) = nanmean(Y0) + std1*randn(sum(stillnan),1);
+        return
+    else
+        keyboard;
+        rethrow(ME);
+    end
+end
+% figure; plot(X0,Y0,'o'); hold on; plot(Xintp,Yintp,'^')
 
 NoInt = length(Yintp);
-std1 = 0.02;
-Y0(Xmissing) = Yintp + std1*randn(NoInt,1);
-% figure; plot(X0,Y0,'o');
-% hold on 
-% plot(X,Y);
-% 
-% bdryInd = diff(Xmissing);
-% bdryInd(bdryInd ==1) = 0; % dY = 1, then it is not a boundary point (indexed at the 2nd one)
-% bdryInd(bdryInd ~=0) = 1;
-% bdryInd(find(bdryInd == 1) - 1) = 1; 
-% bdryInd = [1;bdryInd];
+Y0(XmissingInd) = Yintp + std1*randn(NoInt,1);
+
+% Y0(XmissingInd) = Yintp;
+% Y0 = addNoise(Y0,XmissingInd);
+
+% because Yintp will be NaN if Xintp is not bounded with at least two
+% non-NaN Y value. Therefore,
+% if there is still nan remain:
+stillnan = isnan(Y0); % if NaN starts from the beginning, Y_intp is NaN, 
+if any(stillnan,'all')
+    [TrueInd,FalseInd] = splitContinue10(stillnan);
+    for i = 1:length(TrueInd)
+        ind_i = TrueInd{i};
+        Ind_ext = [ind_i(1)-1,ind_i(end)+1];
+        isInd_ext_valid = Ind_ext > 0 & Ind_ext <= NoY;
+        NearestValueInd = Ind_ext(isInd_ext_valid);
+        NearestValueAvg = mean(Y0(NearestValueInd)); % find the average of the nearest valid value of Y.
+
+        Y0(ind_i) = NearestValueAvg + std1*randn(length(ind_i),1);
+    end
 end
 
+% and the corresponding Y0 dithered with noise remain nan since NaN + anything = NaN.
+end
+
+function Y = addNoise(Y,Ind)
+% Y must be N by 1 array
+numND = sum(Ind,'all'); % number of noise data
+Y(Ind) = std1*randn(numND,1);
+end
