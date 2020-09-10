e70348d755184550764c1d1db07ffe0a2370fa9b
okatsn <49422823+okatsn@users.noreply.github.com>
2020-09-10 16:51:31 +0800

    Update km2latlon_rough.m
    Add support for array input/output as the km2deg

--- km2latlon_rough.m
+++ km2latlon_rough.m
@@ -3,7 +3,7 @@ function [latitude_span_in_deg,longitude_span_in_deg] = km2latlon_rough(distance
 Re = 6371; %radius of earth
 Rl = Re*cosd(around_what_latitude); % approx. radius of the slice of earth at the input latitude ('around_what_latitude').
 latitude_span_in_deg = km2deg(distance_in_km,'earth'); %360*distance_in_km/(2*pi*Re);
-longitude_span_in_deg = km2deg(distance_in_km,Rl); % 360*distance_in_km/(2*pi*Rl);
+longitude_span_in_deg = arrayfun(@(R,Lat) km2deg(R,Lat), distance_in_km, Rl); % 360*distance_in_km/(2*pi*Rl);
 % there is also a km2rad
 
 end
