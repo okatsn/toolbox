0b1da10fbb72f9dbd0ced0ce1f38662713325084
okatsn <okatsn@gmail.com>
2023-05-01 11:24:01 +0800

    Return if to_delete is empty

--- remove_in.m
+++ remove_in.m
@@ -1,5 +1,9 @@
 % `remove_in(keyword, targetfolder, varargin)` remove files in `targetfolder` where `files = datalist(keyword, targetfolder, varargin{:}).fullpath;`.
 function remove_in(keyword, targetfolder, varargin)
     to_delete = datalist(keyword, targetfolder, varargin{:}).fullpath;
-    delete(to_delete{:}, targetfolder)
+    if isempty(to_delete)
+        warning("Nothing to remove in '%s' matching %s", targetfolder, join(string(varargin)))
+        return;
+    end
+    delete(to_delete{:})
 end
