082c7d7d3319b513b30458c951b6c3d032478a2f
okatsn <okatsn@gmail.com>
2025-09-17 12:13:47 +0800

    Trim white spaces.

--- StochasticProcess/EulerSDE_a.m
+++ StochasticProcess/EulerSDE_a.m
@@ -1,9 +1,9 @@
 % Produce a SDE sample path by numerically solving the Langevin equation in Euler scheme.
-% Example: 
+% Example:
 %     O = EulerSDE_a(D,r,totalTime,Y0,dt, 'FrictionType','wet','PlotTrace',1);
 %     O = EulerSDE_a(D,r,totalTime,Y0,dt,'FrictionType','dry','smooth','1000pt','RemoveSign',1);
 % INPUT:
-%     Required: 
+%     Required:
 %         D: diffusional coefficient
 %     Optional:
 %         r: frictional/damping coefficient
@@ -14,19 +14,19 @@
 %         FritionalType: Type of Langevin equation
 %         PlotTrace: 1 for plot, 0 not plot.
 %         seed: use pre-saved seed.
-%         save_to: 
-%             save_to =1: save to default folder; 
-%             Alternatively: save_to = 'subfolder1\subsubfolder'; 
-%         smooth:  
+%         save_to:
+%             save_to =1: save to default folder;
+%             Alternatively: save_to = 'subfolder1\subsubfolder';
+%         smooth:
 %             e.g. smooth = '100pt'; % smooth to total 100 points.
 %             e.g. smooth = '0.1sec'; % averaging window is 0.1 second.
 %         MemoryLimit: (2020-06-04)
 %             Manually assign the largest number of elements of the array at a time.
-%             In default the function will automatically decide the number 
+%             In default the function will automatically decide the number
 %             by estimating the available memory of the device at current state.
 %             However, if you use parfor to run this function, the number should
-%             be divided by the number of parfor iterations; otherwise, 
-%             memory might runs out. That is, you should manually set 'MemoryLimit' 
+%             be divided by the number of parfor iterations; otherwise,
+%             memory might runs out. That is, you should manually set 'MemoryLimit'
 %             when the function is executed inside the parfor loop.
 %             (!Hint) Manually assign this value will significantly
 %             speed up if EulerSDE_a is applied to generate a large
@@ -35,32 +35,32 @@
 % Becareful when change the variable name 'traceT';
 %
 % About rng seed:
-% e.g. 
-% % seed = rng; 
-% % rng(seed); 
-% % A1 = cat(2,randn(1,4),randn(1,5)); 
-% % rng(seed); 
-% % A2 = cat(2,randn(1,3),randn(1,3),randn(1,3)); 
+% e.g.
+% % seed = rng;
+% % rng(seed);
+% % A1 = cat(2,randn(1,4),randn(1,5));
+% % rng(seed);
+% % A2 = cat(2,randn(1,3),randn(1,3),randn(1,3));
 % % isequal(A1,A2);
 % % % then you will find out that A1 is completely the same as A2
-        
-% drift term & diffusional term are calculated in the same line. 
+
+% drift term & diffusional term are calculated in the same line.
 % Therefore, it's 2 times faster than EulerSDE_b, but unable to see the
-% influence of drift term in microscale.  
+% influence of drift term in microscale.
 function [ O ] = EulerSDE_a(D,varargin )% EulerSDE_a(D,r,totalTime,Y0,dt)
 default_r = 1; default_totalTime = 10; default_Y0 = 0; default_dt = 1e-3;  default_Friction = 'dry';
 
 errorStruct.identifier = 'Custom:Error';
-default_VH = 0; %default_outputdW=0; 
+default_VH = 0; %default_outputdW=0;
 expected_types = {'dry','wet','viscous', 'Coulomb'};
 valid_types = @(x) any(validatestring(x,expected_types));
-default_plot = 0;   
+default_plot = 0;
 validScalar = @(x) isnumeric(x) && isscalar(x); % if vector or string, then error. %this term is optional.
 valid10 = @(x) (x==1) || (x==0);
 p = inputParser;
-addRequired(p,'D',validScalar); % note that there shouldn't exist a default term here. 
+addRequired(p,'D',validScalar); % note that there shouldn't exist a default term here.
 %============ below default value is required.
-addOptional(p,'r',default_r); 
+addOptional(p,'r',default_r);
 addOptional(p,'totalTime',default_totalTime,validScalar); %validScalar is optional
 addOptional(p,'Y0',default_Y0,validScalar);
 addOptional(p,'dt',default_dt);
@@ -68,12 +68,12 @@ addParameter(p,'FrictionType',default_Friction,valid_types); %e.g. 'FrictionType
 addParameter(p,'plot',default_plot,valid10); %e.g. 'PlotTrace', 1 ( Name-value pair arguments input)
 % addParameter(p,'OutputdW',default_outputdW); %e.g. 'OutputdW', 1 ( Name-value pair arguments input)
 % addParameter(p,'VelocityHistory',default_VH); %e.g. 'VelocityHistory', 1 ( Name-value pair arguments input)
-addParameter(p,'seed',NaN); 
-addParameter(p,'save_to',0); 
+addParameter(p,'seed',NaN);
+addParameter(p,'save_to',0);
 addParameter(p,'smooth',0);
 addParameter(p,'RemoveSign',0);
 addParameter(p,'information',0);
-addParameter(p,'SaveInplace',0); 
+addParameter(p,'SaveInplace',0);
 addParameter(p,'ConstantForce',0);
 addParameter(p,'TimeElapse',false);
 addParameter(p,'MemoryLimit',0);
@@ -87,8 +87,8 @@ addParameter(p,'MemoryLimit',0);
 parse(p,D,varargin{:});
 results = p.Results;
 r=results.r; totalTime = results.totalTime; Y0 =results.Y0; dt = results.dt; save_to=results.save_to;
-FrictionType = results.FrictionType; 
-smooth = results.smooth; 
+FrictionType = results.FrictionType;
+smooth = results.smooth;
 Plot_= results.plot;
 absFcn = results.RemoveSign;
 showInfo = results.information;
@@ -98,7 +98,7 @@ constantForce = results.ConstantForce;
 Fext_x_dt = constantForce*dt;
 % SteadyStateMean = results.SteadyStateMean;
 % function_ = results.function;
-% FrictionType = results.FrictionType; 
+% FrictionType = results.FrictionType;
 memoryLimit = results.MemoryLimit;
 
 if isequal(memoryLimit,0)
@@ -119,8 +119,8 @@ if ~isequal(SaveInplace,0)
     totalTime_j = min([totalTime,floor(memoryLimit*dt)]);
     iter_j = ceil(totalTime/totalTime_j);
     fprintf('OutputDuration/totalTime = %.2f/%.2f \n',totalTime_j*iter_j,totalTime)
-    Save2 = true;   
-%% create/load matfile       
+    Save2 = true;
+%% create/load matfile
     if isfile(SaveInplace) % if file exist
 %         FileExist = true;
         fprintf("File exists. Append to '%s' \n",SaveInplace);
@@ -135,12 +135,12 @@ if ~isequal(SaveInplace,0)
         Y = [];
         StartPoint = [];
         varNames = {'D','r','dt','constantForce','FrictionType','StartPoint','X','Y'};
-        save(SaveInplace,varNames{:},'-v7.3');               
+        save(SaveInplace,varNames{:},'-v7.3');
         matf = matfile(SaveInplace,'Writable',true);
         fprintf("Create new file to '%s' \n",SaveInplace);
         size_T = [0,0];
     end
-    
+
 %% StartPoint identify the indices where a new stochastic process is appended to the existing matfile
 % This is not necessary, was the feature for manually checking if the
 % appending is done correctly (i.e. X should be continuous, Y should'nt have a gap around any StartPoint.)
@@ -148,10 +148,10 @@ if ~isequal(SaveInplace,0)
 %     if isfield(matf,'StartPoint') % isfield cannot work with fields of matfile
         matf.StartPoint =  [matf.StartPoint, max(size_T) +1]; % previous [StartPoint, EndPoint +1];
 %     else
-%         matf.StartPoint =  [NaN, max(size_T) +1]; % 
+%         matf.StartPoint =  [NaN, max(size_T) +1]; %
 %     end
 
-    
+
 end
 
 
@@ -159,15 +159,15 @@ end
 if smooth~=0
 %     smth_to = regexp(smooth,'(pt|sec)','match');
 %     smooth_parameter = regexp(smooth,'\d+\.?\d*e?[+-]?\d*\.?\d*(?=(pt|sec))','match');
-% '\d+\.?\d*': �Ʀr(�ܤ�1) �p���I(1��0��) �Ʀr(0�ܵL����) 
-% 'e?[+-]?\d*\.?\d*': �r��e(1��0��) (�r�Ŷ�)[+��-](1��0��) �Ʀr(0�ܵL����) �p���I(1��0��) �Ʀr(0�ܵL����) 
+% '\d+\.?\d*': �Ʀr(�ܤ�1) �p���I(1��0��) �Ʀr(0�ܵL����)
+% 'e?[+-]?\d*\.?\d*': �r��e(1��0��) (�r�Ŷ�)[+��-](1��0��) �Ʀr(0�ܵL����) �p���I(1��0��) �Ʀr(0�ܵL����)
 % '(?=(pt|sec))': �᭱�n����pt��sec�~�|�ǰt��
      Sm = regexp(smooth,'(?<sp_>\d*\.?\d*e?[+-]?\d+\.?\d*)(?<st_>pt|sec)','names');
      smooth_parameter = Sm.sp_;
      smth_to = Sm.st_;
      smooth_parameter = str2double(smooth_parameter);
      smooth = 1;% this significantly speed up when smooth_parameter is large.
-%     
+%
 %     assignin('base','smooth_parameter',smooth_parameter);
     switch smth_to
         case 'pt' % smooth to total m point
@@ -187,17 +187,17 @@ if smooth~=0
             T_smth = NaN(m,1); Y_smth = NaN(m,1);
             smooth_info = sprintf('smooth window = %d %s',smooth_parameter,smth_to);
     end
-    
+
 else % not smooth
     if Save2
         m = iter_j;
-        steps=round(totalTime_j/dt); 
+        steps=round(totalTime_j/dt);
     else
         m = 1;
-        steps=round(totalTime/dt); 
+        steps=round(totalTime/dt);
     end
     Nw=steps+1;%Euler method�]n�B(�@n+1����ơA�Ĥ@������l����)
-    
+
     smooth_info = 'original';
 end
 
@@ -274,7 +274,7 @@ for k=1:m
     if Save2
 %         if k==1
 %             if ~FileExist
-%                 save(SaveInplace,'traceT','traceY','-v7.3');               
+%                 save(SaveInplace,'traceT','traceY','-v7.3');
 %                 matf = matfile(SaveInplace,'Writable',true);
 %             end
 %         else
@@ -283,8 +283,8 @@ for k=1:m
             next_idx1 = size_Tk(1) + Nw;
             matf.X(next_idx0:next_idx1,1) = traceT;
             matf.Y(next_idx0:next_idx1,1) = traceY;
-            
-            
+
+
 %         end
         if TimeElapseTR
             [H] = timeLeft1(toc,k,H);
@@ -310,10 +310,10 @@ end
 %         for i=2:Nw
 %         t=t+dt;   y=y+drift(y)*dt+b*dW(i-1);           traceT(i)=t;  traceY(i)=y; %
 %         end
-% 
+%
 %     segY{NoThr_i} = 1;
-% 
-%     
+%
+%
 % end
 
 %% Output
@@ -329,7 +329,7 @@ if smooth==0
 else
   O.X = T_smth;
   O.Y = Y_smth;% All output time series transpose to size = [N,1]
-  
+
 end
 
 if showInfo~=0
@@ -344,8 +344,8 @@ end
 O.smth_tag = smooth_info;
 O.mean_Y_predicted = Y_avg_predicted;
 O.ConstantForce_x_dt = Fext_x_dt;
-% if ~isequal(SteadyStateMean,0)    
-%     O.mean_Y_avg_samplepath_st = 
+% if ~isequal(SteadyStateMean,0)
+%     O.mean_Y_avg_samplepath_st =
 % end
 
 %% Figure plot
@@ -361,13 +361,13 @@ O.ConstantForce_x_dt = Fext_x_dt;
       plot(O.X,abs(O.Y),'.')
       xlabel('time','FontSize',midFont);       ylabel('|Y(t)|','FontSize',midFont);
       ax=gca;  ax.YGrid='on';
-  
+
         pos_x=0; superTitle =sprintf('D=%.2f; \\gamma=%.2f; frictionType: %s',D,r,FrictionType);
         other.FontSize = 18;  %�� �]�w�o��
         pos_y=0.91;  pos = [pos_x pos_y 1-pos_x 1-pos_y] ; %[x_start y_start width height] of textbox.
         other.HorizontalAlignment = 'center'; other.VerticalAlignment = 'bottom';
         annote(superTitle,pos,f1,other);
-  
+
         if isstruct(results.seed)
             info1 ='seed: previously saved';
         else
@@ -379,11 +379,11 @@ O.ConstantForce_x_dt = Fext_x_dt;
         info2 = sprintf('smoothed to %s',smooth_info);
         other_info = {info1,info2}; % can be {info1; info2;...} ';' is for break line.
         other2.VerticalAlignment = 'bottom';
-        annote(other_info,[0 0.02 1 0.1],f1,other2);  
-  
+        annote(other_info,[0 0.02 1 0.1],f1,other2);
+
   figure;
   histogram(dW); title('distribution of dW');
-  
+
   figure;
   std_Y = std(O.Y);
   histogram(O.Y); title('distribution of Y');
@@ -392,9 +392,9 @@ O.ConstantForce_x_dt = Fext_x_dt;
   ylim_ = get(gca,'ylim');
   vline(x1,ylim_,label1);
 
-  
+
   end
-  
+
  %% save variable
   if save_to == 1
       save_to = ''; % save to current path.
@@ -412,12 +412,12 @@ O.ConstantForce_x_dt = Fext_x_dt;
 %         save(target_dir,'O','-v7.3');% for variable larger than 2GB
         save(target_dir,'O');
   end
-    
-  
+
+
 end
 %% annotation
 function annote(text,pos,fig,other)
-dim = pos;  
+dim = pos;
 a=annotation(fig,'textbox',dim,'String','');
 %�إ�annotation�ɥ��w�n���]�wcontanier(�i�H�O��ΡB���B�b�Y)���j�p�M�L��������r
 a.String = text;
@@ -427,11 +427,11 @@ a.LineStyle='none';%�]�w�S���~��
         %Alignment �O�� ��r�����textbox��'��'�B'��'��'�k'��
        a.HorizontalAlignment = other.HorizontalAlignment;
     end
-    
+
     if isfield(other,'VerticalAlignment') % 'middle' (default) | 'top' | 'bottom' | 'baseline' | 'cap'
        a.VerticalAlignment = other.VerticalAlignment;
     end
-    
+
     if isfield(other,'FontSize')
        a.FontSize = other.FontSize;
     end
@@ -439,7 +439,7 @@ a.LineStyle='none';%�]�w�S���~��
 end
 
 function vline(x,ylim_,label_)
-hold on 
+hold on
 
 for i = 1:numel(x)
 line([x{i},x{i}],ylim_,'Color','g');
